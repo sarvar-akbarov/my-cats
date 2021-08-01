752734764f024062948fc679ef90e32f0a7acb81
Sarvar05 <sarvarakbarov0505@gmail.com>
2021-08-01 10:44:01 +0500

    p

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
-TOKEN = '1942434115:AAFafku0P8IOsf4ZoZuTSJvlNwxQCPEeo28'
-# BOT_PATH = '/home/sarvar/Desktop/bots/img2textbot/'
+TOKEN = ''
 
 APP_NAME = "@LoveCats"
 API_KEY = '1abd492b-4d28-4798-936e-4e797c8868df'
-# database
-DB_HOSTNAME = 'localhost'
-DB_USERNAME = 'sarvar'
-DB_PASSWORD = 'Sarvar@akbarov05'
-DB_NAME = 'img2text'
 
 BTN_BREEDS = 'Breeds'
 BTN_RANDOM = 'Get Random Image'
